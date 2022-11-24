836c22593656afe29ca2e27a396833f4fe3aab51
sami0880-kea <sami0880@stud.kea.dk>
2022-11-24 15:31:30 +0100

    Changes by nichl

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Controller {
+    private FileHandler fileHandler;
+    private Database database;
+
+    public Controller() {
+        fileHandler = new FileHandler();
+        database = new Database();
+    }
+
+    public void addMember(String name, String email, int socialSecurityNumber , String address, int phoneNumber, boolean activityType, String membership) {
+        database.addMember(name, email, socialSecurityNumber , address, phoneNumber, activityType, membership);
+    }
+
+    public ArrayList<Member> getMembers() {
+        return database.getMembers();
+    }
+    public void loadMembers() {
+        database.addArrayMember(fileHandler.loadMembers());
+    }
+
+    public void saveMembers() {
+        fileHandler.saveMembers(database.getMembers());
+    }
+
+    public boolean removeMember(int nr) {
+        return database.removeMember(nr);
+    }
 }
