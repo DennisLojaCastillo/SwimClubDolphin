dc8db35c2b301fe68edc863a1932d7a45b7d08a7
amirah2700 <amir2700@hotmail.com>
2022-11-28 10:32:42 +0100

    UNIT TEST AF REMOVEMEMEMBER UDFØRT

--- src/test/java/Database/DatabaseTest.java
+++ src/test/java/Database/DatabaseTest.java
@@ -1,8 +1,13 @@
 package Database;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
+    Database database = new Database();
+    DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @org.junit.jupiter.api.Test
     void addArrayMember() {
@@ -18,5 +23,23 @@ class DatabaseTest {
 
     @org.junit.jupiter.api.Test
     void removeMember() {
+        database.addMember(" Amir", LocalDate.parse("01-01-2001", formatDate), "Amir2700@hotmail.com", "chupapi 21", 9999,
+                true);
+        database.removeMember(1);
+        assertEquals(0, database.getMembers().size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMember2() {
+        database.addMember(" Amir", LocalDate.parse("01-01-2001", formatDate), "Amir2700@hotmail.com", "chupapi 21", 9999,
+                true);
+        database.removeMember(2);
+        assertEquals(1, database.getMembers().size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMember3() {
+        database.removeMember(2);
+        assertEquals(0, database.getMembers().size());
     }
 }
