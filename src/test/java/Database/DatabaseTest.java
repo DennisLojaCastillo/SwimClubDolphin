a110e482f95f706b4836b3747a52c956fb2bb167
DennisLCastillo <55577545+DennisLCastillo@users.noreply.github.com>
2022-11-28 10:13:27 +0100

    unit test in database setup

---
+++ src/test/java/Database/DatabaseTest.java
@@ -0,0 +1,22 @@
+package Database;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @org.junit.jupiter.api.Test
+    void addArrayMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMembers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMember() {
+    }
+}
